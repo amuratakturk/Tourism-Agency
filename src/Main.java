5062c38e4dece8b9f468c74dbfae59f07ea3005e
Abdurrahman Murat Aktürk <muratakturk97@hotmail.com>
2024-06-26 18:30:15 +0300

    sending

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tourism Agency is adloading... ");
+        System.out.println("Tourism Agency is a loading... ");
     }
 }
